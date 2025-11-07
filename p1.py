a91e655f508b5d90def8ad6374e0dc7f97265a89
pruthvi <miatra.pruthvi2022@gmail.com>
2025-11-07 11:59:32 +0530

    first program

---
+++ p1.py
@@ -0,0 +1,7 @@
+#Write a program to swap two numbers without taking a 
+#temporary variable. 
+a=input("Enter Value a")
+b=input("Enter Value b")
+a,b=b,a
+print("Value of A ",a)
+print("Value of B",b)
