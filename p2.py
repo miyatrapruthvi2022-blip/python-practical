7c82026b59580516e63a7cd89fc7c418e3243419
pruthvi <miatra.pruthvi2022@gmail.com>
2025-11-07 12:18:21 +0530

    first program

---
+++ p2.py
@@ -0,0 +1,7 @@
+#Write a program to display sum of two complex numbers.
+a= complex(3,4)
+b=complex(5,3)
+sum=a+b
+print("First Complex Number ",a)
+print("Second Complex Number ",b)
+print("Sum of two complex numbers: ",sum)
